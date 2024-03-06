f6cd9ddc1ea1294190ea6b20f1121137ffd472fe
tulad58 <you@example.com>
2024-03-06 19:24:52 +0300

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from datetime import datetime
 
 
@@ -36,27 +37,28 @@ def parse_date(date_string):
     return date_obj
 
 
-def filter_data(start_time, end_time, aperture, csv_file):
+def filter_data(start_time, end_time, aperture_column, csv_file, apperture):
     start_time = parse_date(start_time)
     end_time = parse_date(end_time)
 
     data = read_csv_data(csv_file)
     filtered_data = [data[0]]
 
+    prev_row = None
     for row in data[1:]:
         timestamp = parse_date(row[1])
         if start_time <= timestamp <= end_time:
-            if len(row) > 2:
+            if prev_row is not None:
                 diff_check = False
-                for i in range(2, len(row)):
-                    if i > 2:
-                        prev_value = float(row[i - 1].replace(",", "."))
-                        curr_value = float(row[i].replace(",", "."))
-                        if abs(curr_value - prev_value) > aperture:
-                            diff_check = True
-                            break
+                for col_index in aperture_column:
+                    curr_value = float(row[col_index].replace(",", "."))
+                    prev_value = float(prev_row[col_index].replace(",", "."))
+                    if abs(curr_value - prev_value) > apperture:
+                        diff_check = True
+                        break
                 if diff_check:
                     filtered_data.append(row)
+        prev_row = row
 
     return filtered_data
 
@@ -70,13 +72,19 @@ def write_csv_data(data, output_file):
 if __name__ == "__main__":
     start_time = "18 августа 2022 г. 00:00:00.000 мсек"
     end_time = "18 августа 2022 г. 08:00:00.000 мсек"
-    aperture = 0.5
+    aperture_column = range(3, 593)
+    apperture = 0.5
     input_csv_file = "786442_Ribbon1 Сводная.csv"
-    output_csv_file = "output_data.csv"
+    output_csv_file = "output_data4.csv"
 
     start = datetime.now()
-    filtered_data = filter_data(start_time, end_time, aperture, input_csv_file)
+    filtered_data = filter_data(
+        start_time, end_time, aperture_column, input_csv_file, apperture
+    )
     write_csv_data(filtered_data, output_csv_file)
+    df = pd.read_csv(output_csv_file, encoding="cp1251", sep=";")
+    df_sorted = df.sort_values(by="Дата и время записи")
+    df_sorted.to_csv("output_sorted.csv", encoding="cp1251", sep=";", index=False)
     end = datetime.now()
 
     print("Время выполнения функции:", end - start)
